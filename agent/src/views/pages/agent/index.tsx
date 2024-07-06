46bb38e93ac1b534dcfcc58d766553ceeb950f74
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-07-06 17:47:36 +0530

    refactor: Update agent table layout, add email column, and remove agent context menu

--- agent/src/views/pages/agent/index.tsx
+++ agent/src/views/pages/agent/index.tsx
@@ -1,12 +1,9 @@
-import { AddIcon } from '@chakra-ui/icons';
-import { Button, Flex, HStack, Text } from '@chakra-ui/react';
-import { useNavigate, useOutlet } from 'react-router-dom';
-import { NAVIGATION } from '../../../config/const';
+import { Flex, HStack, Text } from '@chakra-ui/react';
+import { useOutlet } from 'react-router-dom';
 import AllAgents from './components/AllAgents';
 
 export default function AgentPage() {
 	const outlet = useOutlet();
-	const navigate = useNavigate();
 
 	return (
 		<Flex
@@ -20,15 +17,6 @@ export default function AgentPage() {
 				<Text fontSize={'2xl'} fontWeight={'bold'}>
 					Agents
 				</Text>
-				<Button
-					onClick={() => navigate(`${NAVIGATION.APP}/${NAVIGATION.AGENT}/new`)}
-					variant='outline'
-					size={'sm'}
-					colorScheme='green'
-					leftIcon={<AddIcon />}
-				>
-					Create Agents
-				</Button>
 			</HStack>
 			<AllAgents />
 			{outlet}
